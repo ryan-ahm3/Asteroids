b9433d25e8999cc4dd891d1d0a6e3ee4d3804ad4
ryan-ahm3 <ryankani411@gmail.com>
2026-02-18 15:53:41 +1100

    implemented ship movement

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    playership = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(1):
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
+        # Draw
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-    
+
+        # Locked to 60 fps
+        dt = clock.tick(60) / 1000 
+
+        
+
 if __name__ == "__main__":
     main()
